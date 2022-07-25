1e1e158b9813193893522fc3374b43b13ef8fc83
Thibault Duplessis <t@lichess.org>
2022-07-25 10:23:04 +0200

    tweak gitignore and publish

--- publish.sh
+++ publish.sh
@@ -7,10 +7,6 @@ echo "Publishing $version"
 sed -i -e "s/\"version\": \".*\"/\"version\": \"$version\"/g" package.json;
 
 npm run test
-npm run compile
-npm run dev
-npm run prod
-npm run sass-prod
 
 git add package.json
 
